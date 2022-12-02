6acc0d1c46158294b6a34e74efcfa83c4b90cf61
jnccd <kobidogao@outlook.com>
2022-12-02 19:48:36 +0100

    Add project files.

---
+++ Dec1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+var theNumbersMason = File.ReadAllText("input.txt").
+    Split("\n\n").
+    Select(x => x.TrimEnd('\n').Split("\n").
+        Select(y => Convert.ToInt32(y)).
+        Sum()).ToArray().
+        OrderByDescending(x => x);
+
+Console.WriteLine($"Chonkiest Boy: {theNumbersMason.First()}, Sum of the Top 3 Chonks: {theNumbersMason.Take(3).Sum()}");
